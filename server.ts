c580817c2df453de159bcb250da927dc26d2a8b5
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2023-07-29 23:19:31 +0300

    fix

--- server.ts
+++
@@ -1,4 +0,0 @@
-const app = require('./src/server.ts');
-const http = require('http');
-
-http.createServer(app).listen(process.env.PORT,console.log(`🚀 Server running on port ${process.env.PORT}!`));
